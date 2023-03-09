01c51b2ca9d997ca4a5e85be090fad7177fed7e9
rykci <yuen.ricky@hotmail.com>
2023-03-09 02:37:57 -0500

    buying phase complete

--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,69 @@
 import { INVALID_MOVE, TurnOrder } from 'boardgame.io/core'
 
-const Bid = ({ G, playerID }, amount) => {
-  if (G.bids[G.highestBidder] >= amount) {
+const PickCard = ({ G, ctx, playerID, events }, card) => {
+  const index = G.players[playerID].cards.indexOf(card)
+  if (index == -1) {
     return INVALID_MOVE
   }
 
-  console.log(`${playerID} BIDDING ${amount}`)
-  G.players[playerID].coins -= amount
-  G.bids[playerID] = amount
+  G.selling[playerID] = card
+  G.players[playerID].cards.splice(index, 1)
+
+  if (!G.selling.includes(0)) {
+    for (let i = 0; i < ctx.numPlayers; i++) {
+      console.log(G.selling)
+      // let lowestCard = Math.min(G.selling.map((price) => Number(price)))
+      // let playerIndex = G.selling.indexOf(lowestCard)
+      // console.log('player', playerIndex, 'sold', lowestCard)
+      // console.log(G.players[playerIndex])
+      // G.players[playerIndex].coins += G.prices.pop()
+      // G.selling[playerIndex] = 31
+    }
+
+    events.endPhase()
+  }
+}
+
+const Bid = ({ G, playerID }, amount) => {
+  if (
+    G.players[playerID].bid + G.players[playerID].coins <=
+    G.players[G.highestBidder].bid
+  ) {
+    return INVALID_MOVE
+  }
+
+  if (G.players[G.highestBidder].bid >= amount) {
+    return INVALID_MOVE
+  }
+
+  console.log(`PLAYER ${playerID} BIDS ${amount} COIN`)
+  G.players[playerID].coins -= amount - G.players[playerID].bid
+  G.players[playerID].bid = amount
   G.highestBidder = playerID
 }
 
-const Pass = ({ G, playerID, events }) => {
-  G.bidding[playerID] = false
-  G.players[playerID].coins += Math.floor(G.bids[playerID] / 2)
-  G.players[playerID].cards.push(G.auction.pop())
+const Pass = ({ G, playerID, events, ctx }) => {
+  console.log(`PLAYER ${playerID} PASSED`)
+  G.players[playerID].hasPassed = true
+  G.players[playerID].coins += Math.floor(G.players[playerID].bid / 2)
+  let pickup = G.auction.pop()
+  G.players[playerID].cards.push(pickup)
+
+  console.log(`PLAYER ${playerID} PICKS UP ${pickup}`)
+  let s = 'AUCTION: [ '
+  for (let i = 0; i < G.auction.length; i++) {
+    s += G.auction[i] + ', '
+  }
+  console.log(s + ']')
 
   if (G.auction.length == 1) {
+    for (let i = 0; i < ctx.numPlayers; i++) {
+      if (G.players[i].hasPassed == false) {
+        G.highestBidder = i
+        G.players[i].cards.push(G.auction.pop())
+        break
+      }
+    }
     events.endPhase()
   }
 }
@@ -28,6 +75,11 @@ export const ForSale = {
     let deck = random
       .Shuffle([...Array(31).keys()].slice(1))
       .slice(30 % ctx.numPlayers)
+
+    let priceDeck = [...Array(16).keys(), ...Array(15).keys()].slice(1)
+    priceDeck[0] = 0
+    priceDeck[16] = 15
+    priceDeck = random.Shuffle(priceDeck).slice(30 % ctx.numPlayers)
 
     let initialAmount = 14
     if (ctx.numPlayers == 5) {
@@ -44,17 +96,41 @@ export const ForSale = {
       players[i] = { coins: initialAmount, cards: [] }
     }
 
-    return { deck, players }
+    return {
+      deck,
+      priceDeck,
+      players,
+      highestBidder: random.Die(ctx.numPlayers) - 1,
+      prices: [null],
+    }
+  },
+
+  turn: {
+    minMoves: 1,
+    maxMoves: 1,
+    order: {
+      first: ({ G }) => parseInt(G.highestBidder),
+      next: ({ G, ctx }) => {
+        for (let i = 1; i <= ctx.numPlayers; i++) {
+          let nextIndex = (parseInt(ctx.currentPlayer) + i) % ctx.numPlayers
+          console.log('checking index:', nextIndex)
+          if (!G.players[nextIndex].hasPassed) {
+            return nextIndex
+          }
+        }
+      },
+    },
   },
 
   phases: {
     buying: {
-      turn: { minMoves: 1, maxMoves: 1 },
       onBegin: ({ G, ctx }) => {
         G.auction = G.deck.splice(0, ctx.numPlayers).sort((a, b) => b - a)
-        G.highestBidder = 0
-        G.bids = new Array(ctx.numPlayers).fill(0)
-        G.bidding = new Array(ctx.numPlayers).fill(true)
+        console.log('AUCTION:', G.auction)
+        for (let i = 0; i < ctx.numPlayers; i++) {
+          G.players[i].bid = 0
+          G.players[i].hasPassed = false
+        }
       },
       start: true,
       moves: { Bid, Pass },
@@ -62,9 +138,40 @@ export const ForSale = {
         return G.deck.length == 0 ? 'selling' : 'buying'
       },
     },
+
+    selling: {
+      onBegin: ({ G, ctx }) => {
+        G.prices = G.priceDeck.splice(0, ctx.numPlayers).sort((a, b) => b - a)
+        G.selling = Array(ctx.numPlayers).fill(0)
+      },
+
+      turn: {
+        stages: {
+          playCard: {
+            moves: { PickCard },
+          },
+        },
+        minMoves: 1,
+        maxMoves: 1,
+        activePlayers: { all: 'playCard' },
+      },
+
+      next: 'selling',
+    },
+  },
+
+  endIf: ({ G, ctx }) => {
+    if (G.prices.length == 0 && G.priceDeck.length == 0) {
+      let coinsArr = Array(ctx.numPlayers)
+      for (let i = 0; i < ctx.numPlayers; i++) {
+        coinsArr[i] = G.players[i].coins
+      }
+      return { winner: coinsArr.findIndex(Math.max(coinsArr)) }
+    }
   },
 }
 
 // TODO:
-// - skip players who passed
-// - start round robin turn order on winner of last round
+// [X] skip players who passed
+// [X] start round robin turn order on winner of last round
+// [ ] selling phase
