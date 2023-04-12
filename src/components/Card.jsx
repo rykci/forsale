d89514604c11b566c413b47ad3a10e2e0a0f7a66
rykci <yuen.ricky@hotmail.com>
2023-04-11 23:26:30 -0400

    add dynamic color class

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function Card({ value }) {
+function Card({ color = 'yellow', value }) {
+  const colorVariants = {
+    yellow: 'bg-gradient-to-tr from-yellow-500 to-yellow-200',
+    green: 'bg-gradient-to-br  from-green-600 to-green-400',
+  }
+
   return (
     <div
-      className={`bg-gradient-to-tr from-yellow-500 to-yellow-200 flex border-2 rounded-lg h-36 w-28 text-center justify-center items-center text-4xl font-bold`}
+      className={`${colorVariants[color]} flex border-2 rounded-lg h-36 w-28 text-center justify-center items-center text-4xl font-bold`}
     >
       <div>{value}</div>
     </div>
